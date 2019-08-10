59461410e0ec6eeb2569d48d7dd0476abd10f85d
Matt Hagner <web@matthagner.com>
2019-08-10 12:06:31 -0500

    Update route to be api/posts

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const posts = require('./routes/posts');
+const express = require("express");
+const posts = require("./routes/posts");
 
 const server = express();
 
 // Allows us to parse the request body
 server.use(express.json());
 
-server.use('/posts', posts);
+server.use("/api/posts", posts);
 
 module.exports = server;
